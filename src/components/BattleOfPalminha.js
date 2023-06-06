19ec5581c7f1ecef30b95251e9340dceffe61447
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-06-06 13:35:51 -0300

    fix

--- src/components/BattleOfPalminha.js
+++ src/components/BattleOfPalminha.js
@@ -3,9 +3,7 @@ import React, { useState } from 'react';
 import './BattleOfPalminha.css';
 
 import image1 from '../images/image1.png';
-import image2 from '../images/image2.jpg';
 import image3 from '../images/image3.jpg';
-import image4 from '../images/image4.jpg';
 import image5 from '../images/image5.png';
 
 const BattleOfPalminha = () => {
